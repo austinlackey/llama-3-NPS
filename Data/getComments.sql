5c0b88d299952ff02abac6ac10e07908f98bbeb4
Austin Lackey <103147811+austinlackey@users.noreply.github.com>
2024-04-26 03:42:53 -0600

    Delete Data directory

--- Data/getComments.sql
+++
@@ -1,3 +0,0 @@
-SELECT        Parks.UnitCode, Parks.Name AS ParkName, VisitationComments.Collecteddate AS Date, VisitationComments.Comments
-FROM            Parks INNER JOIN
-                         VisitationComments ON Parks.ParkId = VisitationComments.ParkID
